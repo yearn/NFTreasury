f821ad64d0625a8ab1b0bfacf01626f49805da20
Marco Worms <marcogworms@gmail.com>
2022-06-26 12:10:05 -0300

    fix hidden footer on mobile

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,7 +13,7 @@ function	Footer(): ReactElement {
 	const router = useRouter();
 	const isDisclaimerPage = disclaimerPathnames.includes(router.pathname);
 	return (
-		<footer className={'hidden flex-row items-center py-8 mx-auto mt-auto w-full max-w-6xl md:flex'}>
+		<footer className={'flex flex-row items-center py-8 mx-auto mt-auto w-full max-w-6xl'}>
 			<Link href={'/what-are-yearn-vaults'}>
 				<p className={`link-with-dot ${isDisclaimerPage ? 'active' : '' }`}>
 					{'disclaimer'}
