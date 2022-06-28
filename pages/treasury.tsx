b962dc59aa627b67fd1554f60235435e9f63209e
Majorfi <major-tom.eth@pm.me>
2022-06-28 12:37:03 +0200

    feat: basic chart setup

--- pages/treasury.tsx
+++ pages/treasury.tsx
@@ -1,122 +1,110 @@
 import	React, {ReactElement}						from	'react';
 import	Link										from	'next/link';
 import	{Card, Button}								from	'@yearn-finance/web-lib/components';
+import	{format}									from	'@yearn-finance/web-lib/utils';
 import	WithShadow									from	'components/WithShadow';
-import {LineChart, Line, Tooltip, XAxis, YAxis,
+import {LineChart, Line, Tooltip, XAxis,
 	ResponsiveContainer} 							from 	'recharts';
+import useWallet from 'contexts/useWallet';
+import useYearn from 'contexts/useYearn';
 
-const mockData: object[] = [
-	{
-		name: 'Page A',
-		uv: 4000,
-		pv: 2400,
-		amt: 2400
-	},
-	{
-		name: 'Page B',
-		uv: 3000,
-		pv: 1398,
-		amt: 2210
-	},
-	{
-		name: 'Page C',
-		uv: 2000,
-		pv: 3000,
-		amt: 2290
-	},
-	{
-		name: 'Page D',
-		uv: 2780,
-		pv: 3908,
-		amt: 2000
-	},
-	{
-		name: 'Page E',
-		uv: 1890,
-		pv: 4800,
-		amt: 2181
-	},
-	{
-		name: 'Page F',
-		uv: 2390,
-		pv: 3800,
-		amt: 2500
-	},
-	{
-		name: 'Page G',
-		uv: 3490,
-		pv: 4300,
-		amt: 2100
-	}
-];
+function Chart({data}: {data: any[]}): ReactElement {
+	const ActiveDot = (props: any): ReactElement => {
+		const	{cx, cy} = props;
+		return (
+			<svg x={cx - 5} y={cy - 5} width={10} height={10} fill={'none'} viewBox={'0 0 10 10'}>
+				<rect x={'1'} y={'1'} width={'8'} height={'8'} fill={'white'} stroke={'black'} strokeWidth={'2'}/>
+			</svg>
+		);
+	};
+	const CustomTooltip = (props: any): ReactElement | null => {
+		const	{active, payload} = props;
+		if (active && payload && payload.length) {
+			return (
+				<div className={'custom-tooltip'}>
+					<p className={'label'}>{`$ ${format.amount(payload[0].value, 2, 2)}`}</p>
+				</div>
+			);
+		}
+		
+		return null;
+	};
 
-function Chart(): ReactElement {
+
 	return (
-		// <div className={'border-2 border-solid p-0'}>
-		<ResponsiveContainer width={'100%'} height={300}>
-			<LineChart data={mockData}>
-				<XAxis dataKey={'name'} stroke={'#000'} tick={false} axisLine={false}/>
-				<YAxis orientation={'right'} stroke={'#000'} tickCount={2} axisLine={false}/>
-				<Tooltip />
-				<Line
-					type={'monotone'}
-					dataKey={'pv'}
-					stroke={'#000'}
-					strokeWidth={2}
-					dot={false}
-				/>
-			</LineChart>
-		</ResponsiveContainer>
-		// </div>
+		<div>
+			<ResponsiveContainer width={608} height={192}>
+				<LineChart
+					className={'overflow-hidden max-h-[192px] border-2 border-black'}
+					margin={{top: 8, right: 0, left: 0, bottom: 8}}
+					data={(
+						data
+							.sort((a, b): number => a.timestamp - b.timestamp)
+							.map((dayData): {name: string, ['USD Price']: number} => ({
+								name: format.date(dayData.timestamp * 1000),
+								['USD Price']: Number(dayData.outputTokenPriceUSD)
+							})))}>
+					<XAxis dataKey={'name'} stroke={'#000'} tick={false} axisLine={false} height={0} />
+					<Tooltip content={<CustomTooltip />} />
+					<Line
+						activeDot={<ActiveDot />}
+						type={'monotone'}
+						dataKey={'USD Price'}
+						stroke={'#000'}
+						strokeWidth={2}
+						dot={false}
+					/>
+				</LineChart>
+			</ResponsiveContainer>
+		</div>
 	);
 }
 
 function	TreasuryPage(): ReactElement {
+	const	{balances, prices} = useWallet();
+	const	{yvEthData, treasuryData} = useYearn();
+
 	return (
 		<div className={'flex flex-col items-center h-full md:flex-row'}>
 			<WithShadow role={'large'}>
-				<Card className={'flex flex-col justify-between w-full md:w-[700px] md:h-[544px]'}>
-					<div>
+				<Card className={'flex flex-col w-full md:w-[700px] md:h-[488px]'}>
+					<div className={'mb-8'}>
 						<div className={'pb-6 w-full'}>
 							<h2 className={'font-bold'}>{'Your Treasury'}</h2>
 						</div>
-						<div className={'flex flex-wrap justify-between items-center mb-6 md:mb:0'}>
-							<div className={'mb:p-0 px-2 m-0 mb-4 md:p-0'}>
-								<p>
-									{'Holdings, ETH'}
+						<div className={'flex flex-wrap justify-between items-center'}>
+							<div className={'px-2 m-0 md:p-0'}>
+								<p>{'Holdings, ETH'}</p>
+								<p className={'font-bold'}>
+									{balances ? format.amount(balances[process.env.ETH_VAULT_ADDRESS as string]?.normalized || 0, 8, 8) : '-'}
 								</p>
+							</div>
+							<div className={'px-2 m-0 md:p-0'}>
+								<p>{'Holdings, $'}</p>
+								<p className={'font-bold'}>
+									{balances ? format.amount(
+										(balances[process.env.ETH_VAULT_ADDRESS as string]?.normalized || 0)
+										* (prices[process.env.ETH_VAULT_ADDRESS as string]?.normalized || 0),
+										8, 8
+									) : '-'}
+								</p>
+							</div>
+							<div className={'px-2 m-0 md:p-0'}>
+								<p>{'Earnings, ETH'}</p>
 								<p className={'font-bold'}>
 									{'1234.12345678'}
 								</p>
 							</div>
 							<div className={'px-2 m-0 md:p-0'}>
-								<p>
-									{'Holdings, $'}
-								</p>
+								<p>{'Est. Yield, %'}</p>
 								<p className={'font-bold'}>
-									{'51234.12'}
-								</p>
-							</div>
-							<div className={'px-2 m-0 md:p-0'}>
-								<p>
-									{'Earnings, ETH'}
-								</p>
-								<p className={'font-bold'}>
-									{'1234.12345678'}
-								</p>
-							</div>
-							<div className={'px-2 m-0 md:p-0'}>
-								<p>
-									{'Est. Yield, %'}
-								</p>
-								<p className={'font-bold'}>
-									{'12.4'}
+									{yvEthData ? `${format.amount((yvEthData?.apy?.net_apy || 0) * 100, 2, 2)}` : '-'}
 								</p>
 							</div>
 						</div>
 					</div>
-					<Chart />
-					<div className={'flex flex-col justify-start items-start md:flex-row'}>
+					<Chart data={treasuryData?.vaultDailySnapshots || []}/>
+					<div className={'flex flex-col justify-start items-start mt-8 md:flex-row'}>
 						<Link href={'/keep-eth'}>
 							<div>
 								<WithShadow role={'button'}>
@@ -142,25 +130,21 @@ function	TreasuryPage(): ReactElement {
 			</WithShadow>
 			<div className={'mt-8 w-full md:mt-0 md:ml-24'}>
 				<WithShadow role={'large'}>
-					<Card className={'flex flex-col justify-between w-full h-[544px] md:w-[400px]'}>
+					<Card className={'flex flex-col justify-between w-full h-[488px] md:w-[400px]'}>
 						<div className={'space-y-6'}>
 							<div className={'pb-6 w-full'}>
 								<h2 className={'font-bold'}>{'Your Wallet'}</h2>
 							</div>
 							<div>
-								<p>
-									{'ETH'}
-								</p>
+								<p>{'ETH'}</p>
 								<p className={'font-bold'}>
-									{'1234.12345678'}
+									{balances ? format.amount(balances[process.env.ETH_TOKEN_ADDRESS as string]?.normalized || 0, 8, 8) : '-'}
 								</p>
 							</div>
 							<div>
-								<p>
-									{'USDC'}
-								</p>
+								<p>{'USDC'}</p>
 								<p className={'font-bold'}>
-									{'51234.12'}
+									{balances ? format.amount(balances[process.env.USDC_TOKEN_ADDRESS as string]?.normalized || 0, 2, 6) : '-'}
 								</p>
 							</div>
 						</div>
